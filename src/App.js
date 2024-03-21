ec2ac1db5cc7ac5b0aa8c374ffe869d4bcb7f48d
MaralTach <yoloten.maral@gmail.com>
2024-03-21 23:12:32 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,34 +1,92 @@
-import {categories} from "./data";
-import  {products} from "./data";
-// import "./index.css";
-console.log(products)
-console.log(categories)
+// import {categories} from "./data";
+// import  {products} from "./data";
+// // import "./index.css";
+// console.log(products)
+// console.log(categories)
+
+
+// function App() {
+//   const arr = products.map((item)=> item.image)
+//   console.log(arr)
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+     
+//           <h1 className="title">Product List</h1>   
+//       </header>
+
+      
+    
+//       <div className="container">
+//         {
+//           products.map((item,index)=>
+//           <div key={index} >
+           
+//             <div className="img-container">
+//               <img src={item.image} alt="" />
+//             </div>
+//             {/* <p className="imgTitle">{item.title}</p> */}
+//             {/* <p className="productPrice">{item.price}$</p> */}
+//             {/* <p className="productDesc">{item.description}</p> */}
+//           </div>
+//           )
+//         }
+       
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+/* ********************************** */
+/*             2nji cozum             */
+/* ********************************** */
+// import './App.css';
+import { products } from './data';
+import { categories } from './data';
+import React, {useState} from 'react'; // import et
 
 function App() {
-  const arr = products.map((item)=> item.image)
-  console.log(arr)
+
+const [selectedCategory, setSelectedCategory] = useState('all') 
+
+const filterCategory = (category) => {
+  if(category === 'all') {
+    return products
+  }
+ return products.filter(product => product.category === category)
+}
+
+const clickCategory = (category) => {
+  setSelectedCategory(category)
+  console.log(category)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-     
-          <h1 className="title">Product List</h1>   
-      </header>
-    
       <div className="container">
-        {
-          products.map((item,index)=>
-          <div key={index} >
-           
-            <div className="img-container">
-              <img src={item.image} alt="" />
-            </div>
-            {/* <p className="imgTitle">{item.title}</p> */}
-            {/* <p className="productPrice">{item.price}$</p> */}
-            {/* <p className="productDesc">{item.description}</p> */}
-          </div>
-          )
-        }
-       
+        <h1>Product List</h1>
+        <div className="categories">
+          {
+            categories.map((category,index) => 
+              <button key={index} onClick={() => clickCategory(category)}>{category}</button>
+            )
+          }
+        </div>
+        <div className="products">
+          {
+            filterCategory(selectedCategory).map((product,index) => 
+              <div key={index} className="product">
+                <img src={product.image} alt="images" />
+                <p className= "price">{product.price + '$'}</p>
+                <p className="title">{product.title}</p>
+              </div>
+            )
+          }
+        </div>
       </div>
     </div>
   );
