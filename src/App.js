e9d68adc52762de62c081843757d5389ccea5816
MaralTach <yoloten.maral@gmail.com>
2024-03-22 19:12:46 +0300

    finished work

--- src/App.js
+++ src/App.js
@@ -4,24 +4,21 @@
 // console.log(products)
 // console.log(categories)
 
-
 // function App() {
 //   const arr = products.map((item)=> item.image)
 //   console.log(arr)
 //   return (
 //     <div className="App">
 //       <header className="App-header">
-     
-//           <h1 className="title">Product List</h1>   
+
+//           <h1 className="title">Product List</h1>
 //       </header>
 
-      
-    
 //       <div className="container">
 //         {
 //           products.map((item,index)=>
 //           <div key={index} >
-           
+
 //             <div className="img-container">
 //               <img src={item.image} alt="" />
 //             </div>
@@ -31,7 +28,7 @@
 //           </div>
 //           )
 //         }
-       
+
 //       </div>
 //     </div>
 //   );
@@ -39,55 +36,62 @@
 
 // export default App;
 
-
-
 /* ********************************** */
 /*             2nji cozum             */
 /* ********************************** */
 // import './App.css';
-import { products } from './data';
-import { categories } from './data';
-import React, {useState} from 'react'; // import et
+import { products } from "./data";
+import { categories } from "./data";
+import React, { useState } from "react"; //!React Hook
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("all");
 
-const [selectedCategory, setSelectedCategory] = useState('all') 
+  const filterCategory = (category) => {
+    if (category === "all") {
+      return products;
+    }
+    return products.filter((product) => product.category === category);
+  };
 
-const filterCategory = (category) => {
-  if(category === 'all') {
-    return products
-  }
- return products.filter(product => product.category === category)
-}
-
-const clickCategory = (category) => {
-  setSelectedCategory(category)
-  console.log(category)
-}
+  const clickCategory = (category) => {
+    setSelectedCategory(category);
+    console.log(category);
+  };
 
   return (
     <div className="App">
       <div className="container">
         <h1>Product List</h1>
         <div className="categories">
-          {
-            categories.map((category,index) => 
-              <button key={index} onClick={() => clickCategory(category)}>{category}</button>
-            )
-          }
+          {categories.map((category, index) => (
+            <button key={index} onClick={() => clickCategory(category)}>
+              {category}
+            </button>
+          ))}
         </div>
         <div className="products">
-          {
-            filterCategory(selectedCategory).map((product,index) => 
-              <div key={index} className="product">
-                <img src={product.image} alt="images" />
-                <p className= "price">{product.price + '$'}</p>
-                <p className="title">{product.title}</p>
-              </div>
-            )
-          }
+          {filterCategory(selectedCategory).map((product, index) => (
+            <div key={index} className="product">
+
+            
+             <div className="imageSec">
+              <img src={product.image} alt="images" />
+             </div>
+
+             <p className="title">{product.title}</p>
+             
+              <h3 className="price">
+                <span className="prColor">Price:</span>
+                {product.price + "$"}
+              </h3>
+             
+              
+            </div>
+          ))}
         </div>
       </div>
+     
     </div>
   );
 }
